2a724ddbdbfb2a5f8cb51b3f9ff653cb615fb78f
lz10hnr <923622583@qq.com>
2017-03-07 10:58:11 +0800

    测试类

--- app/src/main/java/cn/usian/android_day01/MainActivity.java
+++ app/src/main/java/cn/usian/android_day01/MainActivity.java
@@ -2,6 +2,7 @@ package cn.usian.android_day01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("abc","zhuac");
     }
 }
--- app/src/main/java/cn/usian/android_day01/aaa.java
+++ app/src/main/java/cn/usian/android_day01/aaa.java
@@ -1,7 +1,13 @@
 package cn.usian.android_day01;
 
+import android.util.Log;
+
 /**
  * Created by TuLing on 2017/3/7.
  */
 public class aaa {
+
+    public void aaa(){
+        Log.i("abc","张三。。。");
+    }
 }
