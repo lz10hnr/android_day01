6c1ceb764d91330fe6dc20a548fbf5c3d13d0d76
lz10hnr <923622583@qq.com>
2017-03-07 10:05:37 +0800

    Initial commit

---
+++ app/src/main/java/cn/usian/android_day01/Math.java
@@ -0,0 +1,28 @@
+package cn.usian.android_day01;
+
+/**
+ * Created by TuLing on 2017/3/6.
+ */
+public class Math {
+
+    /**
+     * 求两个数的和
+     * @param num1 第一个数
+     * @param num2 第二个数
+     * @return num1+num2
+     */
+    public int sum(int num1,int num2){
+        return num1+num2;
+    }
+
+    /**
+     * 求两个数的商
+     * @param num1 第一个数
+     * @param num2 第二个数
+     * @return num1/num2
+     */
+    public int divide(int num1,int num2){
+        return 0;
+    }
+
+}
---
+++ app/src/test/java/cn/usian/android_day01/MathTest.java
@@ -0,0 +1,39 @@
+package cn.usian.android_day01;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by TuLing on 2017/3/6.
+ */
+public class MathTest {
+
+    private Math math;
+    private Math math2 = null;
+
+    /**
+     * 在该方法中做程序的初始化
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+
+        math = new Math();
+    }
+
+    @Test
+    public void testSum() throws Exception {
+
+//        Assert.assertEquals("实际值与期望值不一致",3,math.sum(1,2));
+
+//        Assert.assertFalse("yyyy",1!=2);
+//        Assert.assertTrue("zzzz",1==1);
+//        Assert.assertNotNull("该对象为空",math2);
+//        Assert.assertSame("两个对象不相等",math,math2);
+    }
+
+    @Test
+    public void testDivide() throws Exception {
+
+    }
+}
